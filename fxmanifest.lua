069e5b9afcd76900173bf835ef4265ba4df0692f
Ludaro1024 <waschmaschvanlu@gmail.com>
2025-08-03 10:56:14 +0200

    renamed it to Appercy and added server folder and shared folder

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,23 +1,22 @@
 fx_version("cerulean")
 game("gta5")
 lua54("yes")
-
-author("Ludaro")
-description("Ludaro Vue Boilerplate")
+author("Appercy")
+description("Appercy Vue Boilerplate")
 version("1.0.0")
 
 client_scripts({
-	"client/*.lua",
+	"client/**/*",
 })
 
 server_scripts({
 	"@oxmysql/lib/MySQL.lua",
-	"server/*.lua",
+	"server/**/*",
 })
 
 shared_scripts({
-	"@ox_lib/init.lua",
-	"config/*.lua"
+	-- "@ox_lib/init.lua",
+	"shared/**/*",
 })
 
 ui_page({
